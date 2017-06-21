8311bb298e61358f3d4b4b2dc1f8aa6b6aaa3b33
Ashley Durbin <ashleydurbin@Ashleys-MacBook-Air.local>
2017-06-21 13:27:49 -0700

    Wednesdy-update

--- main.js
+++ main.js
@@ -76,21 +76,37 @@ function coolChart() {
     chartData.push(allArray[i].clicks);
     chartLabel.push(allArray[i].name);
   }
+
+
   var ctx = document.getElementById("myChart").getContext('2d');
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
       labels: chartLabel,
       datasets: [{
-        label: '# of clicks',
+        label: 'Buss Mall - Products Clicked ',
         data: chartData,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 1)',
           'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
+          'rgba(255, 206, 88, 0.3)',
+          'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          'rgba(255, 159, 64, 0.3)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 88, 0.3)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.3)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 88, 0.3)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.3)',
+          'rgba(255, 99, 132,1)',
+          'rgba(54, 162, 235, 0.2)',
         ],
         borderColor: [
           'rgba(255,99,132,1)',
@@ -98,9 +114,24 @@ function coolChart() {
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
         ],
-        borderWidth: 1
+        borderWidth: 2
       }]
     },
     options: {
